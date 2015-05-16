6a040e577fab506867bfd5bf21a16a5279d66572
Waniusza <janusz.sokolow@gmail.com>
2015-05-16 08:33:03 +0200

    Przygotowanie view do developerki. Zrobione: - uruchomiony szablon - uruchomiony JSF oraz CDI - wprowadzona struktura plikow TODO : - internacjonalizacja do poprawy

---
+++ PracticePortal/src/main/java/com/gut/pactice/entity/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gut.pactice.entity;
+
+import javax.persistence.Entity;
+import lombok.ToString;
+
+/**
+ *
+ * @author janusz
+ */
+@Entity
+@ToString
+public class NewClass {
+    
+}
